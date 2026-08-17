7eef3db0547eea57bcbc069328562d0111c28c39
Juan Diego Lopez Galvis <jdilopezg@unbosque.edu.co>
2026-08-16 22:16:08 -0500

    Correccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://juancho.github.io", 
+  site: "https://JuanchoLopezGalvis.github.io", 
   devToolbar: {
     enabled: false,
   },
