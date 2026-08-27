62695a432b83df3a11ff56707d085b2079f35625
nivin <nivinvysakh@icloud.com>
2026-08-27 16:20:04 +0530

    fix(config): remove subpath base to resolve root domain asset loading on nivinvysakh.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nivinvysakh.github.io",
+  site: "https://nivinvysakh.github.io/astro-tui-portfolio/",
   devToolbar: {
     enabled: false,
   },
